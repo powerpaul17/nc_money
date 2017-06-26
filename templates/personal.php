693f331a9699b3672612f6cdf4ddbab36620ade2
powerpaul17 <powerpaul17@users.noreply.github.com>
2017-06-26 22:25:13 +0200

    remove multi-currency support (not ready yet)

--- templates/personal.php
+++ templates/personal.php
@@ -8,10 +8,10 @@
 ?>
 
 <!-- <div class="section">
-  <h2><?php p($l->t('Money')); ?></h2>
+  <h2><?php //p($l->t('Money')); ?></h2>
   <div id="money-settings">
-    <label><?php p($l->t('Account summary currency')); ?>:</label>
-    <input type="text" placeholder="<?php p($l->t('Account summary currency')); ?>"><?php echo $accountSummaryCurrency; ?></input>
+    <label><?php //p($l->t('Account summary currency')); ?>:</label>
+    <input type="text" placeholder="<?php //p($l->t('Account summary currency')); ?>"><?php //echo $accountSummaryCurrency; ?></input>
     <ui-select title="Choose a currency" theme="select2" tagging tagging-label="false">
       <ui-select-match placeholder="{{ ctrl.t.placeholderCurrency }}">{{$select.selected}}</ui-select-match>
       <ui-select-choices repeat="currency in ctrl.availableCurrencies | filter: $select.search">
