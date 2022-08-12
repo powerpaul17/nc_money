d77e70daed9598eb70c0bedc56f491baebf0d364
paultirk <paultirk@paultirk.com>
2022-08-12 22:03:31 +0000

    add pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import './main.css';
 
 import App from './App.vue';
 
 const app = createApp(App);
+
+const pinia = createPinia();
+app.use(pinia);
 
 app.mount('#content');
