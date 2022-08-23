628119db010d6d779b7a9fb7f45798d344c6044b
paultirk <paultirk@paultirk.com>
2022-08-23 11:01:53 +0200

    add seamless input component

---
+++ src/components/SeamlessInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <input
+    class="border-0 hover:border w-full bg-inherit"
+    type="text"
+    :placeholder="placeholder"
+    v-model.lazy="inputValue"
+    @change="handleValueChange"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      placeholder: {
+        type: String,
+        default: ''
+      }
+    },
+    emits: ['value-changed'],
+    data() {
+      return {
+        inputValue: this.value
+      };
+    },
+    watch: {
+      value() {
+        this.inputValue = this.value;
+      }
+    },
+    methods: {
+      handleValueChange() {
+        this.$emit('value-changed', this.inputValue);
+      }
+    }
+  });
+</script>
