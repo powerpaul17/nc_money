85016802d9ca3f4cc9924944c9fe75eb567248af
paultirk <paultirk@paultirk.com>
2022-08-24 23:32:34 +0200

    add currency input component

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <SeamlessInput
+    :value="currencyValue"
+    :placeholder="placeholder"
+    class="text-right transition-all duration-300 ease-in-out"
+    @value-changed="handleValueChanged"
+  ></SeamlessInput>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  import SeamlessInput from './SeamlessInput.vue';
+
+  export default defineComponent({
+    props: {
+      value: {
+        type: Number,
+        required: true
+      },
+      placeholder: {
+        type: String,
+        default: ''
+      }
+    },
+    emits: ['value-changed'],
+    data() {
+      return {
+        currencyValue: ''
+      };
+    },
+    watch: {
+      formattedValue() {
+        this.currencyValue = this.formattedValue;
+      }
+    },
+    computed: {
+      formattedValue() {
+        return this.value.toFixed(2);
+      }
+    },
+    methods: {
+      handleValueChanged(newValue: string) {
+        this.currencyValue = this.formattedValue;
+
+        const newNumber = Number(newValue);
+        if (!Number.isNaN(newNumber)) {
+          this.$emit('value-changed', newNumber);
+        }
+      }
+    },
+    mounted() {
+      this.currencyValue = this.formattedValue;
+    },
+    components: { SeamlessInput }
+  });
+</script>
