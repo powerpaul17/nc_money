87974f25b539b60d67369917edee42c8cd15c34d
paultirk <paultirk@paultirk.com>
2022-08-25 19:29:03 +0000

    add split list item component

---
+++ src/components/SplitListItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex items-center [&>*]:mx-2">
+    <div></div>
+    <div class="flex-auto">
+      <SeamlessInput
+        placeholder="Description..."
+        :value="split.description"
+        @value-changed="handleDescriptionChanged"
+      ></SeamlessInput>
+    </div>
+    <div>
+      <AccountSelect
+        :account-id="split.destAccountId"
+        :excludedAccountIds="excludedAccountIds"
+        @account-changed="handleDestinationAccountChanged"
+      ></AccountSelect>
+    </div>
+    <div class="flex-shrink-0">
+      <CurrencyInput
+        :value="split.value"
+        placeholder="Value..."
+        @value-changed="handleValueChanged"
+      ></CurrencyInput>
+    </div>
+    <div class="icon-delete" @click="handleDeleteSplit"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, type PropType } from 'vue';
+
+  import { type Split, useTransactionStore } from '../stores/transactionStore';
+
+  import CurrencyInput from './CurrencyInput.vue';
+  import AccountSelect from './AccountSelect.vue';
+  import SeamlessInput from './SeamlessInput.vue';
+
+  export default defineComponent({
+    props: {
+      split: {
+        type: Object as PropType<Split>,
+        required: true
+      },
+      excludedAccountIds: {
+        type: Object as PropType<Array<number>>,
+        default: []
+      }
+    },
+    emits: ['split-deleted'],
+    methods: {
+      async handleValueChanged(value: number) {
+        this.split.value = value;
+        await this.handleSplitChanged();
+      },
+      async handleDeleteSplit() {
+        this.$emit('split-deleted', this.split);
+      },
+      async handleDescriptionChanged(description: string) {
+        this.split.description = description;
+        await this.handleSplitChanged();
+      },
+      async handleDestinationAccountChanged(accountId: number) {
+        this.split.destAccountId = accountId;
+        await this.handleSplitChanged();
+      },
+      async handleSplitChanged() {
+        await this.transactionStore.updateSplit(this.split);
+      }
+    },
+    setup() {
+      const transactionStore = useTransactionStore();
+      return { transactionStore };
+    },
+    components: { CurrencyInput, AccountSelect, SeamlessInput }
+  });
+</script>
