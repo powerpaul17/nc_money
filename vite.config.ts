432383d65bcbafae64c0752c8cec889348f40c73
paultirk <paultirk@paultirk.com>
2022-09-14 16:25:01 +0000

    include nextcloud's vue repo (vue3 branch) & adapt build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
 import vueI18N from '@intlify/vite-plugin-vue-i18n';
+
+const vueDocsPlugin = {
+  name: 'vue-docs',
+  load(id) {
+    if (!/vue&type=docs/.test(id)) {
+      return null;
+    }
+    return 'export default {}';
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -17,7 +29,30 @@ export default defineConfig(({ mode }) => {
       minify: mode === 'development' ? false : 'esbuild',
       sourcemap: mode === 'development' ? true : false
     },
+    define: {
+      TRANSLATIONS: 'Array.from([])'
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use 'sass:math';
+            @import './src/vendor/nextcloud-vue/src/assets/variables';
+            @import './src/vendor/nextcloud-vue/src/assets/material-icons';
+          `
+        }
+      }
+    },
+    resolve: {
+      alias: [
+        {
+          find: /vue-material-design-icons\/(.*)/,
+          replacement: resolve('node_modules/vue-material-design-icons') + '/$1'
+        }
+      ]
+    },
     plugins: [
+      vueDocsPlugin,
       vue(),
       vueI18N({
         compositionOnly: false,
