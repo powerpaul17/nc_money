3bc99a851a50f699f046d6900a2ff87ae462c1cf
paultirk <paultirk@paultirk.com>
2022-11-08 23:13:34 +0100

    add some tests for number formatting

--- src/utils/numberUtils.spec.ts
+++ src/utils/numberUtils.spec.ts
@@ -4,6 +4,51 @@ import { NumberUtils } from './numberUtils';
 
 describe('NumberUtils', () => {
 
+  describe('formatNumber', () => {
+
+    it('should format the number', () => {
+      expect(NumberUtils.formatNumber(123456.789, {}))
+        .to.be.equal('123 456.79');
+    });
+
+    it('should use the specified decimal separator', () => {
+      expect(NumberUtils.formatNumber(123456.789, { decimalSeparator: ',' }))
+        .to.be.equal('123 456,79');
+    });
+
+    it('should use the specified group separator', () => {
+      expect(NumberUtils.formatNumber(123456.789, { groupSeparator: ',' }))
+        .to.be.equal('123,456.79');
+    });
+
+    it('should show the desired number of decimals', () => {
+      expect(NumberUtils.formatNumber(123456.789, { decimals: 5 }))
+        .to.be.equal('123 456.78900');
+    });
+
+    it('should round a number correctly', () => {
+      expect(NumberUtils.formatNumber(123456.789, { decimals: 1 }))
+        .to.be.equal('123 456.8');
+
+      expect(NumberUtils.formatNumber(123456.749, { decimals: 1 }))
+        .to.be.equal('123 456.7');
+    });
+
+    it('should group the digits correctly', () => {
+      expect(NumberUtils.formatNumber(123456.789, { groupBy: 2 }))
+        .to.be.equal('12 34 56.79');
+
+      expect(NumberUtils.formatNumber(123456.789, { groupBy: 4 }))
+        .to.be.equal('12 3456.79');
+    });
+
+    it('should not group number if groupBy is 0', () => {
+      expect(NumberUtils.formatNumber(123456.789, { groupBy: 0 }))
+        .to.be.equal('123456.79');
+    });
+
+  });
+
   describe('parseNumber', () => {
 
     it('should return a number of a string', () => {
