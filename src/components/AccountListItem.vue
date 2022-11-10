deb53921d2f55c5f9c98a95f9238a649321e2053
paultirk <paultirk@paultirk.com>
2022-11-10 14:08:43 +0100

    add missing settingStore to account list item

--- src/components/AccountListItem.vue
+++ src/components/AccountListItem.vue
@@ -61,6 +61,11 @@
         }
       }
     },
-    components: { CurrencyText }
+    components: { CurrencyText },
+    setup() {
+      return {
+        settingStore: useSettingStore()
+      };
+    }
   });
 </script>
