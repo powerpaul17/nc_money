48097b061af2b5e935d165594b469ddd64ca7d1c
paultirk <paultirk@paultirk.com>
2022-11-29 20:11:10 +0000

    use nextcloud vue components for content container

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <AppNavigation />
+  <NcContent app-name="money">
+    <AppNavigation />
 
-  <div id="app-content">
-    <router-view />
-  </div>
+    <NcAppContent>
+      <router-view />
+    </NcAppContent>
 
-  <router-view name="sidebar" />
+    <router-view name="sidebar" />
+  </NcContent>
 </template>
 
 <script setup lang="ts">
+
+  import NcContent from '@nextcloud/vue/dist/Components/NcContent';
+  import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent';
 
   import AppNavigation from './components/AppNavigation.vue';
 
