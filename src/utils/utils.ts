059db2c26ab1c4bd128f7aa52700a73276a0c313
paultirk <paultirk@paultirk.com>
2023-01-31 08:41:53 +0000

    add missing utils class

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+import { ArrayUtils } from './arrayUtils';
+
+export class Utils {
+
+  public static getValueOfCSSVar(cssVar: string): string {
+    return getComputedStyle(document.documentElement)
+      .getPropertyValue(cssVar);
+  }
+
+  public static convertHexToRgba(hexColor: string, opacity = 1.0): string {
+    const hexColorString = hexColor.toUpperCase().replace('#', '');
+
+    const hexColorValues = ArrayUtils.chunk(hexColorString.split(''), 2).map(s => s.join(''));
+    if (hexColorValues.length !== 3) throw new Error('expected 3 hex values');
+
+    return `rgba(${Number(`0x${hexColorValues[0]}`)}, ${Number(`0x${hexColorValues[1]}`)}, ${Number(`0x${hexColorValues[2]}`)}, ${opacity})`;
+  }
+
+}
