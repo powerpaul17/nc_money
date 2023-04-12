5c92c4013cc34f4ac3ab775abb38b4b1e7b602c9
paultirk <paultirk@paultirk.com>
2023-04-12 18:57:21 +0000

    add vue component for dashboard widget

---
+++ src/components/dashboard-widgets/OverviewWidget.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- src/main-dashboard.ts
+++ src/main-dashboard.ts
@@ -1,2 +1,14 @@
+import Vue from 'vue';
+
 import './css/dashboard.css';
 
+import OverviewWidget from './components/dashboard-widgets/OverviewWidget.vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  OCA.Dashboard.register('money-overview-widget', (el) => {
+    const View = Vue.extend(OverviewWidget);
+    new View({
+      propsData: {}
+    }).$mount(el);
+  });
+});
