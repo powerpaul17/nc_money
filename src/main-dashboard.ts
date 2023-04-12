3e8507c33470cc16a312225fa1f7644873a44d96
paultirk <paultirk@paultirk.com>
2023-04-12 18:57:21 +0000

    add pinia store to dashboard widget entry point

--- src/main-dashboard.ts
+++ src/main-dashboard.ts
@@ -1,4 +1,8 @@
 import Vue from 'vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
+
+import './l10n';
+import './chartjs';
 
 import './css/dashboard.css';
 
@@ -6,9 +10,13 @@ import OverviewWidget from './components/dashboard-widgets/OverviewWidget.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   OCA.Dashboard.register('money-overview-widget', (el) => {
+    Vue.use(PiniaVuePlugin);
+    const pinia = createPinia();
+
     const View = Vue.extend(OverviewWidget);
     new View({
-      propsData: {}
+      propsData: {},
+      pinia
     }).$mount(el);
   });
 });
