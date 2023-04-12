50a337aa20ccd1e9c7885205d22ad8584f516011
paultirk <paultirk@paultirk.com>
2023-04-12 22:33:42 +0200

    add assets/liabilities chart to dashboard widget

--- src/components/dashboard-widgets/OverviewWidget.vue
+++ src/components/dashboard-widgets/OverviewWidget.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <LineChart :data="equityChartData" :title="t('money', 'Equity')" />
+    <LineChart :data="assetsChartData" :title="t('money', 'Assets')" />
+    <LineChart :data="liabilitiesChartData" :title="t('money', 'Liabilities')" />
   </div>
 </template>
 
 <script setup lang="ts">
 
   import dayjs from 'dayjs';
+  import colors from 'tailwindcss/colors';
 
   import { computed, onBeforeMount } from 'vue';
 
@@ -60,6 +63,75 @@
       labels: data.map(d => d.label),
       datasets: [
         {
+          values: data.map(d => d.value)
+        }
+      ]
+    };
+  });
+
+  const assetsChartData = computed((): LineChartData => {
+    const currentDate = dayjs();
+
+    const data = GraphDataUtils.createBackwardsCalculatedGraphData({
+      initialValue: {
+        label: currentDate.format('MMM'),
+        value: accountStore.assetsBalance
+      },
+      numberOfPoints: 6,
+      callback: (num, value) => {
+        const date = currentDate.subtract(num, 'months');
+
+        return {
+          label: date.subtract(1, 'month').format('MMM'),
+          value: value - accountStore.getSummaryByType(
+            AccountTypeType.ASSET,
+            date.year(),
+            date.month() + 1
+          )
+        }
+      }
+    });
+
+
+    return {
+      labels: data.map(d => d.label),
+      datasets: [
+        {
+          color: colors.lime[500],
+          values: data.map(d => d.value)
+        }
+      ]
+    };
+  });
+
+  const liabilitiesChartData = computed((): LineChartData => {
+    const currentDate = dayjs();
+
+    const data = GraphDataUtils.createBackwardsCalculatedGraphData({
+      initialValue: {
+        label: currentDate.format('MMM'),
+        value: accountStore.liabilitiesBalance
+      },
+      numberOfPoints: 6,
+      callback: (num, value) => {
+        const date = currentDate.subtract(num, 'months');
+
+        return {
+          label: date.subtract(1, 'month').format('MMM'),
+          value: value - accountStore.getSummaryByType(
+            AccountTypeType.LIABILITY,
+            date.year(),
+            date.month() + 1
+          )
+        }
+      }
+    });
+
+    return {
+      labels: data.map(d => d.label),
+      datasets: [
+        {
+          color: colors.orange[500],
           values: data.map(d => d.value)
         }
       ]
