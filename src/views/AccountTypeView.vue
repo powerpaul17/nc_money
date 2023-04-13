fea288e04e2fffe1f95dcec67effa6d8c26742b6
paultirk <paultirk@paultirk.com>
2023-04-13 19:21:34 +0000

    add time line graph to account type view

--- src/views/AccountTypeView.vue
+++ src/views/AccountTypeView.vue
@@ -1,7 +1,88 @@
 <template>
-  <div />
+  <div class="m-8">
+    <Chart :title="accountType ? accountType.name : ''">
+      <LineChart v-if="!isMonthlyAccountType" :data="lineChartData" />
+      <BarChart v-else :data="barChartData" />
+    </Chart>
+  </div>
 </template>
 
 <script setup lang="ts">
+
+  import { computed } from 'vue';
+
+  import { useAccountTypeStore } from '../stores/accountTypeStore';
+  import { useAccountStore } from '../stores/accountStore';
+
+  import { useSettingStore } from '../stores/settingStore';
+
+  import Chart from '../components/dashboard/Chart.vue';
+  import LineChart, { type Data as LineChartData } from '../components/charts/LineChart.vue';
+  import BarChart from '../components/charts/BarChart.vue';
+
+  import { GraphDataUtils } from '../utils/graphDataUtils';
+  import { AccountTypeUtils } from '../utils/accountTypeUtils';
+
+  const accountTypeStore = useAccountTypeStore();
+  const accountStore = useAccountStore();
+
+  const settingStore = useSettingStore();
+
+  const props = defineProps({
+    accountTypeType: {
+      type: Number,
+      required: true
+    }
+  });
+
+  const accountType = computed(() => {
+    return accountTypeStore.getByType(props.accountTypeType);
+  });
+
+  const isMonthlyAccountType = computed(() => {
+    return AccountTypeUtils.isMonthlyAccount(props.accountTypeType);
+  });
+
+  const isInvertedAccount = computed(() => {
+    return settingStore.useInvertedAccounts && AccountTypeUtils.isInvertedAccount(props.accountTypeType);
+  });
+
+  const lineChartData = computed((): LineChartData => {
+    const inversionFactor = isInvertedAccount.value ? -1 : 1;
+
+    const data = GraphDataUtils.createLineGraphData({
+      startValue: (accountType.value?.balance ?? 0.0) * inversionFactor,
+      callback: (date) => {
+        return  accountStore.getSummaryByType(
+          props.accountTypeType,
+          date.year(),
+          date.month() + 1
+        ) * inversionFactor
+      }
+    })
+
+    return {
+      labels: data.map(d => d.label),
+      datasets: [
+        {
+          values: data.map(d => d.value)
+        }
+      ]
+    }
+  });
+
+  const barChartData = computed(() => {
+    return GraphDataUtils.createBarGraphData({
+      callback: (date) => {
+        const summary = accountStore.getSummaryByType(
+          props.accountTypeType,
+          date.year(),
+          date.month() + 1
+        );
+
+        return isInvertedAccount.value ? summary * -1 : summary;
+      }
+    })
+  });
 
 </script>
