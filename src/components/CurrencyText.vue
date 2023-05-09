03ac1389940d6bdd0ab8182c537d1eb92ecd14c6
paultirk <paultirk@paultirk.com>
2023-05-09 23:53:35 +0200

    use composition api in currency text

--- src/components/CurrencyText.vue
+++ src/components/CurrencyText.vue
@@ -16,98 +16,90 @@
   </div>
 </template>
 
-<script lang="ts">
-  import { defineComponent } from 'vue';
+<script setup lang="ts">
+  import { computed, ref, watch, type Ref } from 'vue';
 
   import { useSettingStore } from '../stores/settingStore';
 
   import { NumberUtils } from '../utils/numberUtils';
 
-  export default defineComponent({
-    props: {
-      value: {
-        type: Number,
-        required: true
-      },
-      animation: {
-        type: Boolean,
-        default: false
-      },
-      invertedValue: {
-        type: Boolean,
-        default: false
-      }
+  const settingStore = useSettingStore();
+
+  const props = defineProps({
+    value: {
+      type: Number,
+      required: true
     },
-    data() {
-      return {
-        animationClass: null as string | null,
-        shownValue: this.value
-      };
+    animation: {
+      type: Boolean,
+      default: false
     },
-    watch: {
-      value(newValue, oldValue) {
-        if (!this.animation) {
-          this.shownValue = newValue;
-          return;
-        }
-
-        if (newValue < oldValue !== this.invertedValue) {
-          this.animationClass = 'change-down';
-        } else {
-          this.animationClass = 'change-up';
-        }
-
-        setTimeout(() => {
-          this.animationClass = null;
-        }, 300);
-
-        const difference = newValue - oldValue;
-        const duration = 500;
-        const changePerMillisecond = difference / duration;
-
-        let startTimestamp: number | undefined;
-
-        const callback: FrameRequestCallback = (timestamp) => {
-          if (startTimestamp == undefined) {
-            startTimestamp = timestamp;
-          }
-          const elapsed = timestamp - startTimestamp;
-
-          this.shownValue = oldValue + changePerMillisecond * elapsed;
-
-          if (
-            (difference > 0 && this.shownValue < newValue) ||
-            (difference < 0 && this.shownValue > newValue)
-          ) {
-            window.requestAnimationFrame(callback);
-          } else {
-            this.shownValue = newValue;
-          }
-        };
-
-        window.requestAnimationFrame(callback);
-      }
-    },
-    computed: {
-      formattedValue() {
-        return NumberUtils.formatNumber(this.shownValue, {
-          decimals: this.settingStore.numberFormat_decimals,
-          decimalSeparator: this.settingStore.numberFormat_decimalSeparator,
-          groupBy: this.settingStore.numberFormat_groupBy,
-          groupSeparator: this.settingStore.numberFormat_groupSeparator,
-          invertedValue: this.invertedValue
-        });
-      }
-    },
-    setup() {
-      return {
-        settingStore: useSettingStore()
-      };
+    invertedValue: {
+      type: Boolean,
+      default: false
     }
   });
+
+  const animationClass: Ref<string|null> = ref(null);
+  const shownValue = ref(props.value);
+
+  watch(() => props.value, (newValue, oldValue) => {
+    if (!props.animation) {
+      shownValue.value = newValue;
+      return;
+    }
+
+    if (newValue < oldValue !== props.invertedValue) {
+      animationClass.value = 'change-down';
+    } else {
+      animationClass.value = 'change-up';
+    }
+
+    setTimeout(() => {
+      animationClass.value = null;
+    }, 300);
+
+    const difference = newValue - oldValue;
+    const duration = 500;
+    const changePerMillisecond = difference / duration;
+
+    let startTimestamp: number | undefined;
+
+    const callback: FrameRequestCallback = (timestamp) => {
+      if (startTimestamp == undefined) {
+        startTimestamp = timestamp;
+      }
+      const elapsed = timestamp - startTimestamp;
+
+      shownValue.value = oldValue + changePerMillisecond * elapsed;
+
+      if (
+        (difference > 0 && shownValue.value < newValue) ||
+        (difference < 0 && shownValue.value > newValue)
+      ) {
+        window.requestAnimationFrame(callback);
+      } else {
+        shownValue.value = newValue;
+      }
+    };
+
+    window.requestAnimationFrame(callback);
+  });
+
+  const formattedValue = computed(() => {
+    return NumberUtils.formatNumber(shownValue.value, {
+      decimals: settingStore.numberFormat_decimals,
+      decimalSeparator: settingStore.numberFormat_decimalSeparator,
+      groupBy: settingStore.numberFormat_groupBy,
+      groupSeparator: settingStore.numberFormat_groupSeparator,
+      invertedValue: props.invertedValue
+    });
+  });
+
 </script>
 
 <style>
+
   .change-up {
     font-size: 110% !important;
     color: lightgreen !important;
@@ -117,4 +109,5 @@
     font-size: 90% !important;
     color: red !important;
   }
+
 </style>
