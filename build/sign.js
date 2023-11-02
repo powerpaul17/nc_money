5af63d652ba96db8243b98c04a5922cc59058de2
paultirk <paultirk@paultirk.com>
2023-11-02 20:32:53 +0000

    add script for writing app key/cert into file & sign the app

---
+++ build/sign.js
@@ -0,0 +1,33 @@
+const {spawnSync} = require('child_process');
+const fs = require('fs');
+
+function writeEnvToFile(envName, fileName) {
+  const value = process.env[envName];
+  if (!value) throw new Error('env variable has no value:' + envName);
+
+  fs.writeFileSync(fileName, value);
+}
+
+console.log('writing key/cert file');
+
+writeEnvToFile('APP_PRIVATE_KEY', '/tmp/money.key');
+writeEnvToFile('APP_CERTIFICATE', '/tmp/money.crt');
+
+console.log('signing app');
+
+try {
+  const occSign = spawnSync('php', [
+    '../../occ',
+    'integrity:sign-app',
+    '--path', 'custom_apps/money/build/dist',
+    '--privateKey', '/tmp/money.key',
+    '--certificate', '/tmp/money.crt'
+  ])
+
+  console.log(occSign.output[1]?.toString());
+} finally {
+  console.log('removing key/cert file');
+
+  fs.unlinkSync('/tmp/money.key');
+  fs.unlinkSync('/tmp/money.crt');
+}
