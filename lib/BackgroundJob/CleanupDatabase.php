fedd07904c539e9d54b664bcb90e18263eb16b23
paultirk <paultirk@paultirk.com>
2024-01-16 09:14:34 +0000

    use executeStatement instead of deprecated executeUpdate

--- lib/BackgroundJob/CleanupDatabase.php
+++ lib/BackgroundJob/CleanupDatabase.php
@@ -28,23 +28,23 @@ class CleanupDatabase extends TimedJob {
     // Remove splits with value = 0
 
     $sql = 'DELETE FROM *PREFIX*money_splits WHERE value = 0;';
-    $deletedEntries = $this->connection->executeUpdate($sql);
+    $deletedEntries = $this->connection->executeStatement($sql);
     $this->logger->debug("$deletedEntries split(s) with zero value deleted", ['app' => 'Money']);
 
     // Remove orphaned splits (not belonging to any account or transaction)
 
     $sql = 'DELETE FROM *PREFIX*money_splits WHERE id IN (SELECT * FROM (SELECT a.id FROM *PREFIX*money_splits a WHERE NOT EXISTS (SELECT null FROM *PREFIX*money_transactions b WHERE a.transaction_id = b.id)) AS p)';
-    $deletedEntries = $this->connection->executeUpdate($sql);
+    $deletedEntries = $this->connection->executeStatement($sql);
     $this->logger->debug("$deletedEntries orphaned split(s) deleted", ['app' => 'Money']);
 
     $sql = 'DELETE FROM *PREFIX*money_splits WHERE id IN (SELECT * FROM (SELECT a.id FROM *PREFIX*money_splits a WHERE NOT EXISTS (SELECT null FROM *PREFIX*money_accounts b WHERE a.dest_account_id = b.id)) AS p)';
-    $deletedEntries = $this->connection->executeUpdate($sql);
+    $deletedEntries = $this->connection->executeStatement($sql);
     $this->logger->debug("$deletedEntries orphaned split(s) deleted", ['app' => 'Money']);
 
     // Remove orphaned transactions (without any splits)
 
     $sql = 'DELETE FROM *PREFIX*money_transactions WHERE id IN (SELECT * FROM (SELECT a.id FROM *PREFIX*money_transactions a WHERE NOT EXISTS (SELECT null FROM *PREFIX*money_splits b WHERE b.transaction_id = a.id)) AS p)';
-    $deletedEntries = $this->connection->executeUpdate($sql);
+    $deletedEntries = $this->connection->executeStatement($sql);
     $this->logger->debug("$deletedEntries orphaned transaction(s) deleted", ['app' => 'Money']);
   }
 
