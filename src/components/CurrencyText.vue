2d5d4d8a4a8c0e7fd1a37340eb068e73db9cbb3d
paultirk <paultirk@paultirk.com>
2024-01-16 09:14:34 +0000

    format CurrencyText.vue

--- src/components/CurrencyText.vue
+++ src/components/CurrencyText.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="flex items-baseline">
     <span
-      class="
-        grow
-        bg-inherit
-        text-right
-        text-inherit
-        transition-all duration-300 ease-in-out
-      "
+      class="grow bg-inherit text-right text-inherit transition-all duration-300 ease-in-out"
       :class="animationClass"
-    >{{ formattedValue }}</span>
+    >
+      {{ formattedValue }}
+    </span>
     <span class="ml-2 shrink-0 text-xs">
       <slot name="suffix" />
     </span>
@@ -40,51 +36,54 @@
     }
   });
 
-  const animationClass: Ref<string|null> = ref(null);
+  const animationClass: Ref<string | null> = ref(null);
   const shownValue = ref(props.value);
 
-  watch(() => props.value, (newValue, oldValue) => {
-    if (!props.animation) {
-      shownValue.value = newValue;
-      return;
-    }
-
-    if (newValue < oldValue !== props.invertedValue) {
-      animationClass.value = 'change-down';
-    } else {
-      animationClass.value = 'change-up';
-    }
-
-    setTimeout(() => {
-      animationClass.value = null;
-    }, 300);
-
-    const difference = newValue - oldValue;
-    const duration = 500;
-    const changePerMillisecond = difference / duration;
-
-    let startTimestamp: number | undefined;
-
-    const callback: FrameRequestCallback = (timestamp) => {
-      if (startTimestamp == undefined) {
-        startTimestamp = timestamp;
-      }
-      const elapsed = timestamp - startTimestamp;
-
-      shownValue.value = oldValue + changePerMillisecond * elapsed;
-
-      if (
-        (difference > 0 && shownValue.value < newValue) ||
-        (difference < 0 && shownValue.value > newValue)
-      ) {
-        window.requestAnimationFrame(callback);
-      } else {
+  watch(
+    () => props.value,
+    (newValue, oldValue) => {
+      if (!props.animation) {
         shownValue.value = newValue;
+        return;
       }
-    };
 
-    window.requestAnimationFrame(callback);
-  });
+      if (newValue < oldValue !== props.invertedValue) {
+        animationClass.value = 'change-down';
+      } else {
+        animationClass.value = 'change-up';
+      }
+
+      setTimeout(() => {
+        animationClass.value = null;
+      }, 300);
+
+      const difference = newValue - oldValue;
+      const duration = 500;
+      const changePerMillisecond = difference / duration;
+
+      let startTimestamp: number | undefined;
+
+      const callback: FrameRequestCallback = (timestamp) => {
+        if (startTimestamp == undefined) {
+          startTimestamp = timestamp;
+        }
+        const elapsed = timestamp - startTimestamp;
+
+        shownValue.value = oldValue + changePerMillisecond * elapsed;
+
+        if (
+          (difference > 0 && shownValue.value < newValue) ||
+          (difference < 0 && shownValue.value > newValue)
+        ) {
+          window.requestAnimationFrame(callback);
+        } else {
+          shownValue.value = newValue;
+        }
+      };
+
+      window.requestAnimationFrame(callback);
+    }
+  );
 
   const formattedValue = computed(() => {
     return NumberUtils.formatNumber(shownValue.value, {
@@ -95,11 +94,9 @@
       invertedValue: props.invertedValue
     });
   });
-
 </script>
 
 <style>
-
   .change-up {
     font-size: 110% !important;
     color: lightgreen !important;
@@ -109,5 +106,4 @@
     font-size: 90% !important;
     color: red !important;
   }
-
 </style>
