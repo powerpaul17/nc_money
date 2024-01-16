9a519b4ebf5bcf91f31e9a0aa1b1b284ccaf237d
paultirk <paultirk@paultirk.com>
2024-01-16 09:14:34 +0000

    format AccountSelect.vue

--- src/components/AccountSelect.vue
+++ src/components/AccountSelect.vue
@@ -8,7 +8,12 @@
     :append-to-body="true"
     :filter-by="
       (option, label, search) => {
-        return [ label, option.description ].join(' ').toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) >= 0;
+        return (
+          [label, option.description]
+            .join(' ')
+            .toLocaleLowerCase()
+            .indexOf(search.toLocaleLowerCase()) >= 0
+        );
       }
     "
     :placeholder="`-- ${t('money', 'No Account')} --`"
@@ -18,85 +23,93 @@
         {{ account.name }}
       </div>
       <div class="truncate text-xs">
-        <span class="uppercase">{{ AccountTypeUtils.getAbbreviationOfAccountType(account.type) }}</span> {{ account.description ? `· ${account.description}` : '' }}
+        <span class="uppercase">{{
+          AccountTypeUtils.getAbbreviationOfAccountType(account.type)
+        }}</span>
+        {{ account.description ? `· ${account.description}` : '' }}
       </div>
     </template>
 
     <template #selected-option="account">
       <div class="truncate">
-        {{ account.name }} · <span class="text-xs uppercase">{{ AccountTypeUtils.getAbbreviationOfAccountType(account.type) }}</span>
+        {{ account.name }} ·
+        <span class="text-xs uppercase">{{
+          AccountTypeUtils.getAbbreviationOfAccountType(account.type)
+        }}</span>
       </div>
     </template>
 
     <template #no-options="{ search }">
       <div class="truncate">
-        {{ search ? t('money', 'No account found...') : t('money', 'No account available...') }}
+        {{
+          search
+            ? t('money', 'No account found...')
+            : t('money', 'No account available...')
+        }}
       </div>
     </template>
   </VueSelect>
 </template>
 
 <style>
+  .vs__dropdown-menu {
+    --vs-border-width: 2px;
+    --vs-border-radius: var(--border-radius-large);
+  }
 
-.vs__dropdown-menu {
-  --vs-border-width: 2px;
-  --vs-border-radius: var(--border-radius-large);
-}
+  .vs__dropdown-menu {
+    --vs-border-color: var(--color-primary-element);
+  }
 
-.vs__dropdown-menu {
-  --vs-border-color: var(--color-primary-element);
-}
+  .account-select {
+    --vs-dropdown-bg: var(--color-main-background);
 
-.account-select {
-  --vs-dropdown-bg: var(--color-main-background);
+    --vs-dropdown-option--active-bg: var(--color-background-hover);
+    --vs-dropdown-option--active-color: var(--color-main-text);
 
-  --vs-dropdown-option--active-bg: var(--color-background-hover);
-  --vs-dropdown-option--active-color: var(--color-main-text);
+    --vs-search-input-bg: transparent;
 
-  --vs-search-input-bg: transparent;
+    --vs-selected-color: var(--color-main-text);
 
-  --vs-selected-color: var(--color-main-text);
+    --vs-border-color: transparent;
+    --vs-border-width: 2px;
+    --vs-border-radius: var(--border-radius-large);
 
-  --vs-border-color: transparent;
-  --vs-border-width: 2px;
-  --vs-border-radius: var(--border-radius-large);
+    --vs-dropdown-color: var(--color-main-text);
 
-  --vs-dropdown-color: var(--color-main-text);
+    --vs-disabled-bg: transparent;
+  }
 
-  --vs-disabled-bg: transparent;
-}
+  .account-select.vs--open {
+    --vs-search-input-bg: var(--color-main-background);
+  }
 
-.account-select.vs--open {
-  --vs-search-input-bg: var(--color-main-background);
-}
+  .account-select:hover:not(.vs--disabled),
+  .account-select:focus-within {
+    --vs-border-color: var(--color-primary-element);
+  }
 
-.account-select:hover:not(.vs--disabled),
-.account-select:focus-within {
-  --vs-border-color: var(--color-primary-element);
-}
+  .account-select .vs__selected-options {
+    overflow: hidden;
+    flex-wrap: nowrap;
+  }
 
-.account-select .vs__selected-options {
-  overflow: hidden;
-  flex-wrap: nowrap;
-}
+  .account-select .vs__selected {
+    overflow: hidden;
+    margin: 0;
+  }
 
-.account-select .vs__selected {
-  overflow: hidden;
-  margin: 0;
-}
+  .account-select.vs--open .vs__selected {
+    position: unset;
+  }
 
-.account-select.vs--open .vs__selected {
-  position: unset;
-}
+  .account-select .vs__dropdown-toggle {
+    padding: 0;
+  }
 
-.account-select .vs__dropdown-toggle {
-  padding: 0;
-}
-
-.account-select .vs__search {
-  margin: 0;
-}
-
+  .account-select .vs__search {
+    margin: 0;
+  }
 </style>
 
 <script setup lang="ts">
@@ -108,11 +121,11 @@
 
   import VueSelect from 'vue-select';
 
-  const  accountStore = useAccountStore();
+  const accountStore = useAccountStore();
 
   const props = withDefaults(
     defineProps<{
-      accountId?: number|null;
+      accountId?: number | null;
       editable?: boolean;
       excludedAccountIds?: Array<number>;
     }>(),
@@ -123,11 +136,13 @@
     }
   );
 
-  const emit = defineEmits([ 'account-changed' ]);
+  const emit = defineEmits(['account-changed']);
 
   const selectedAccount = computed({
     get() {
-      const selectedAccount = accounts.value.find((a) => a.id === props.accountId);
+      const selectedAccount = accounts.value.find(
+        (a) => a.id === props.accountId
+      );
       return selectedAccount ?? null;
     },
     set(newSelectedAccount) {
@@ -136,15 +151,14 @@
   });
 
   const accounts = computed((): Array<Account> => {
-    return accountStore.accounts.value.filter(
-      (a) => !props.excludedAccountIds?.includes(a.id)
-    ).sort((a1, a2) => {
-      if (a1.type === a2.type) {
-        return a1.name.localeCompare(a2.name);
-      }
+    return accountStore.accounts.value
+      .filter((a) => !props.excludedAccountIds?.includes(a.id))
+      .sort((a1, a2) => {
+        if (a1.type === a2.type) {
+          return a1.name.localeCompare(a2.name);
+        }
 
-      return a1.type - a2.type;
-    });
+        return a1.type - a2.type;
+      });
   });
-
 </script>
