57eda4379e579e54e69679f5c0566738c8ea88e9
paultirk <paultirk@paultirk.com>
2024-02-09 07:43:48 +0000

    format SeamlessInput.vue

--- src/components/SeamlessInput.vue
+++ src/components/SeamlessInput.vue
@@ -2,27 +2,18 @@
   <label class="flex flex-col items-baseline">
     {{ label }}
     <input
-      class="
-        w-full
-        overflow-hidden text-ellipsis
-        border-transparent
-        bg-inherit
-        hover:border-primary-element
-        focus:border-primary-element
-        disabled:text-text-maxcontrast disabled:hover:border-transparent
-      "
+      class="w-full overflow-hidden text-ellipsis border-transparent bg-inherit hover:border-primary-element focus:border-primary-element disabled:text-text-maxcontrast disabled:hover:border-transparent"
       type="text"
       :placeholder="placeholder + '...'"
       :disabled="disabled"
       v-model="inputValue"
       @change="handleValueChange"
       @focus="$event.target.select()"
-    >
+    />
   </label>
 </template>
 
 <script setup lang="ts">
-
   import { ref, watch } from 'vue';
 
   const props = defineProps({
@@ -45,19 +36,21 @@
   });
 
   const emit = defineEmits<{
-    (event: 'value-changed', value: string): void
+    (event: 'value-changed', value: string): void;
   }>();
 
   const inputValue = ref(props.value);
 
-  watch(() => props.value, () => {
-    inputValue.value = props.value;
-  });
+  watch(
+    () => props.value,
+    () => {
+      inputValue.value = props.value;
+    }
+  );
 
   function handleValueChange(): void {
     const newValue = inputValue.value;
     inputValue.value = props.value;
     emit('value-changed', newValue);
   }
-
 </script>
