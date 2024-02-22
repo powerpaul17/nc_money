36684d041bb403e81ac26aa693d3797da8186adf
paultirk <paultirk@paultirk.com>
2024-02-22 21:04:48 +0000

    add convert rate to split input

--- src/components/SplitInput.vue
+++ src/components/SplitInput.vue
@@ -12,11 +12,14 @@
       @account-changed="handleSplitAccountChanged(split, $event)"
     />
     <CurrencyInput
-      :value="split.value"
+      :value="value"
       :editable="editable"
       :placeholder="t('money', 'Value')"
       :inverted-value="invertedValue"
+      :enable-convert-rate="enableConvertRate"
+      :convert-rate="convertRate / split.convertRate"
       @value-changed="handleSplitValueChanged(split, $event)"
+      @convert-rate-changed="handleSplitConvertRateChanged(split, $event)"
     />
     <div class="col-span-2">
       <SeamlessInput
@@ -29,7 +32,7 @@
 </template>
 
 <script setup lang="ts">
-  import type { PropType } from 'vue';
+  import { computed, type PropType } from 'vue';
 
   import AccountSelect from './AccountSelect.vue';
   import CurrencyInput from './CurrencyInput.vue';
@@ -38,9 +41,12 @@
   import type { Split } from '../stores/splitStore';
   import { useSplitService } from '../services/splitService';
 
-  const splitService = useSplitService();
+  import { useAccountStore } from '../stores/accountStore';
 
-  defineProps({
+  const splitService = useSplitService();
+  const accountStore = useAccountStore();
+
+  const props = defineProps({
     bookId: {
       type: Number,
       required: true
@@ -60,6 +66,40 @@
     editable: {
       type: Boolean,
       default: true
+    },
+    sourceAccountId: {
+      type: Number,
+      default: undefined
+    },
+    convertRate: {
+      type: Number,
+      default: 1.0
+    }
+  });
+
+  const sourceAccount = computed(() => {
+    return props.sourceAccountId
+      ? accountStore.getById(props.sourceAccountId)
+      : undefined;
+  });
+
+  const destAccount = computed(() => {
+    return accountStore.getById(props.split.destAccountId);
+  });
+
+  const enableConvertRate = computed(() => {
+    return (
+      !!sourceAccount.value &&
+      !!destAccount.value &&
+      sourceAccount.value.currency !== destAccount.value.currency
+    );
+  });
+
+  const value = computed(() => {
+    if (enableConvertRate.value) {
+      return (props.split.value * props.split.convertRate) / props.convertRate;
+    } else {
+      return props.split.value;
     }
   });
 
@@ -88,6 +128,14 @@
     description: string
   ): Promise<void> {
     split.description = description;
+    await handleSplitChanged(split);
+  }
+
+  async function handleSplitConvertRateChanged(
+    split: Split,
+    convertRate: number
+  ): Promise<void> {
+    split.convertRate = props.convertRate / convertRate;
     await handleSplitChanged(split);
   }
 
