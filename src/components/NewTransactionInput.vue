9b2e15476c30a65580efe78f629e1adb94f32cae
paultirk <paultirk@paultirk.com>
2024-02-22 21:30:19 +0000

    also enable convert rate dialog when it is different to 1.0

--- src/components/NewTransactionInput.vue
+++ src/components/NewTransactionInput.vue
@@ -103,7 +103,8 @@
   });
 
   const enableConvertRate = computed(() => {
-    return !!account.value && !!destAccount.value && account.value.currency !== destAccount.value.currency;
+    return NumberUtils.areNotEqual(convertRate.value, 1.0) ||
+      !!account.value && !!destAccount.value && account.value.currency !== destAccount.value.currency;
   });
 
   const isValid = computed(() => {
