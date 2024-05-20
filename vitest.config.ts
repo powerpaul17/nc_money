4395911e9c2405baf9b523c71acbe2bf0e2f3e98
paultirk <paultirk@paultirk.com>
2024-05-20 21:14:45 +0200

    use excludes from vitest config defaults

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 import vue2 from '@vitejs/plugin-vue2';
 
 export default defineConfig({
-  plugins: [
-    vue2()
-  ],
+  plugins: [vue2()],
   test: {
     environment: 'jsdom',
     globals: true,
     coverage: {
       provider: 'istanbul'
     },
-    exclude: [
-      'node_modules',
-      'src/vendor'
-    ],
+    exclude: [...configDefaults.exclude, 'src/vendor'],
     alias: [
       // remove this when migration to vue 3 is complete
       {
