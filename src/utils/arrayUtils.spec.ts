5203b3679df2f9ad73ca3ce4cea3fb939a32704e
paultirk <paultirk@paultirk.com>
2024-05-23 21:01:26 +0000

    format arrayUtils.spec.ts

--- src/utils/arrayUtils.spec.ts
+++ src/utils/arrayUtils.spec.ts
@@ -3,51 +3,47 @@ import { describe, expect, it } from 'vitest';
 import { ArrayUtils } from './arrayUtils';
 
 describe('ArrayUtils', () => {
-
   describe('chunk', () => {
-
     it('should split an array into chunks', () => {
-      const inputArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+      const inputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
       expect(ArrayUtils.chunk(inputArray, 1)).to.deep.equal([
-        [ 0 ],
-        [ 1 ],
-        [ 2 ],
-        [ 3 ],
-        [ 4 ],
-        [ 5 ],
-        [ 6 ],
-        [ 7 ],
-        [ 8 ],
-        [ 9 ]
+        [0],
+        [1],
+        [2],
+        [3],
+        [4],
+        [5],
+        [6],
+        [7],
+        [8],
+        [9]
       ]);
 
       expect(ArrayUtils.chunk(inputArray, 3)).to.deep.equal([
-        [ 0, 1, 2 ],
-        [ 3, 4, 5 ],
-        [ 6, 7, 8 ],
-        [ 9 ]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [9]
       ]);
 
       expect(ArrayUtils.chunk(inputArray, 4)).to.deep.equal([
-        [ 0, 1, 2, 3 ],
-        [ 4, 5, 6, 7 ],
-        [ 8, 9 ]
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9]
       ]);
 
       expect(ArrayUtils.chunk(inputArray, 20)).to.deep.equal([
-        [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
       ]);
     });
 
     it('should not split an array if chunk size is 0', () => {
-      const inputArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+      const inputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
       expect(ArrayUtils.chunk(inputArray, 0)).to.deep.equal([
-        [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
       ]);
     });
-
   });
-
 });
