60b3c1c00c8acdd6f39d76c545360e987e7226c4
paultirk <paultirk@paultirk.com>
2024-05-23 21:01:26 +0000

    format arrayUtils.ts

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -1,17 +1,15 @@
 export class ArrayUtils {
-
   public static chunk<T>(array: Array<T>, chunkSize: number): Array<Array<T>> {
-    if (chunkSize === 0) return [ array ];
+    if (chunkSize === 0) return [array];
 
     return array.reduce<Array<Array<T>>>((chunks, item, index) => {
       return index % chunkSize === 0
-        ? [ ...chunks, [ item ] ]
-        : [ ...chunks.slice(0, -1), [ ...chunks.slice(-1)[0] ?? [], item ] ];
+        ? [...chunks, [item]]
+        : [...chunks.slice(0, -1), [...(chunks.slice(-1)[0] ?? []), item]];
     }, []);
   }
 
   public static createNumberArray(count: number): Array<number> {
     return Array.from(Array(count).keys());
   }
-
 }
