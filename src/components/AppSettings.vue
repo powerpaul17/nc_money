95ddca0dd57e022cbac4db7be1d7440d1997e97b
paultirk <paultirk@paultirk.com>
2024-07-07 20:09:45 +0000

    allow setting income/expense accounts value format to monthly/yearly

--- src/components/AppSettings.vue
+++ src/components/AppSettings.vue
@@ -3,16 +3,57 @@
     <NcCheckboxRadioSwitch v-model="settingStore.useInvertedAccounts.value">
       {{ t('money', 'Show inverted income/liabilities account balances') }}
     </NcCheckboxRadioSwitch>
+
+    <NcSelect
+      v-model="incomeExpenseAccountsValueFormatValue"
+      :options="[
+        {
+          label: t('money', 'Monthly'),
+          value: IncomeExpenseAccountsValueFormat.MONTHLY
+        },
+        {
+          label: t('money', 'Yearly'),
+          value: IncomeExpenseAccountsValueFormat.YEARLY
+        }
+      ]"
+      label="label"
+      >abc</NcSelect
+    >
   </NcAppNavigationSettings>
 </template>
 
 <script setup lang="ts">
+  import { computed } from 'vue';
+
   import { translate as t } from '@nextcloud/l10n';
 
-  import { useSettingStore } from '../stores/settingStore';
+  import {
+    IncomeExpenseAccountsValueFormat,
+    useSettingStore
+  } from '../stores/settingStore';
+
+  import { Utils } from '../utils/utils';
 
   import NcAppNavigationSettings from '@nextcloud/vue/dist/Components/NcAppNavigationSettings.js';
   import NcCheckboxRadioSwitch from '@nextcloud/vue/dist/Components/NcCheckboxRadioSwitch.js';
+  import NcSelect from '@nextcloud/vue/dist/Components/NcSelect.js';
 
   const settingStore = useSettingStore();
+
+  const incomeExpenseAccountsValueFormatValue = computed<{
+    label: string;
+    value: IncomeExpenseAccountsValueFormat;
+  }>({
+    get: () => {
+      const value = settingStore.incomeExpenseAccountsValueFormat.value;
+
+      return {
+        label: t('money', Utils.upperCaseFirstLetter(value)),
+        value
+      };
+    },
+    set: (option) => {
+      settingStore.incomeExpenseAccountsValueFormat.value = option.value;
+    }
+  });
 </script>
