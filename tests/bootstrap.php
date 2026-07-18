81eada60cc1b1a3a48f1f70617f78c0556859537
paultirk <paultirk@paultirk.com>
2026-07-18 23:32:17 +0200

    fix integration tests for NC 34

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,10 @@ define('PHPUNIT_RUN', 1);
 require_once __DIR__.'/../../../lib/base.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+if (isset(\OC::$loader)) {
+  \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+}
+
 \OC_App::loadApp('money');
 
 OC_Hook::clear();
